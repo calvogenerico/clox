94909261e89fa78f6c433671954312668e4779f6
Calvo Generico <miguelfeliped@gmail.com>
2025-07-26 15:48:54 -0300

    local variables working with no edge cases

--- compiler.c
+++ compiler.c
@@ -320,6 +320,9 @@ static int resolveLocal(Compiler* compiler, Token* name) {
     for (int i = compiler->localCount - 1; i >=0; i--) {
         Local* local = &compiler->locals[i];
         if (identifiersEqual(&local->name, name)) {
+            if (local->depth == -1) {
+                error("Can't read local variable in its own initializer.");
+            }
             return i;
         }
     }
@@ -452,7 +455,7 @@ static void addLocal(Token name) {
 
     Local* local = &current->locals[current->localCount++];
     local->name = name;
-    local->depth = current->scopeDepth;
+    local->depth = -1;
 }
 
 static void declareVariable() {
@@ -485,8 +488,13 @@ static uint8_t parseVariable(char* errorMsg) {
     return identifierConstant(&parser.previous);
 }
 
+static void markInitialized() {
+    current->locals[current->localCount - 1].depth = current->scopeDepth;
+}
+
 static void defineVariable(uint8_t global) {
     if (current->scopeDepth > 0) {
+        markInitialized();
         return;
     }
     emitBytes(OP_DEFINE_GLOBAL, global);
