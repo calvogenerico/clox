f4b235b51e300891d5c052055e7fa9f3084d84f2
Calvo Generico <miguelfeliped@gmail.com>
2025-08-02 22:05:50 -0300

    Fixed upvalue index bug

--- compiler.c
+++ compiler.c
@@ -299,7 +299,7 @@ static void endScope() {
     while (current->localCount > 0 &&
            current->locals[current->localCount - 1].depth >
                current->scopeDepth) {
-        if (current->locals[current->localCount].isCaptured) {
+        if (current->locals[current->localCount -1].isCaptured) {
             emitByte(OP_CLOSE_UPVALUE);
         } else {
             emitByte(OP_POP);
